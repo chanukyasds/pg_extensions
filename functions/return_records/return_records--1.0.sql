bea362be90721b72baf74cf262db4b40ad140753
chanukya <86487075+chanukyasds@users.noreply.github.com>
2023-05-16 16:30:08 +0530

    code cleanup

--- functions/return_records/return_records--1.0.sql
+++
@@ -1,6 +0,0 @@
-DROP FUNCTION IF EXISTS return_records(integer[]);
-
-CREATE OR REPLACE FUNCTION return_records(integer[])
-RETURNS SETOF integer
-AS '/extensions/return_records/return_records','return_records'
-LANGUAGE C STRICT;
--- functions/split_array/split_array--1.0.sql
+++
@@ -1,6 +0,0 @@
-DROP FUNCTION IF EXISTS split_array(stud_type[]);
-
-CREATE OR REPLACE FUNCTION split_array(stud_type[])
-RETURNS SETOF stud_type
-AS '/extensions/split_array/split_array','split_array'
-LANGUAGE C STRICT;
